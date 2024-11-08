883d53a30cd5c7685e72d1365a15517f6df388fa
Chaithra30023202 <chaithrakenchanna15@gmail.com>
2024-11-08 09:19:56 +0530

    Update A5.c

--- Classwork-202/Day10/A5.c
+++ Classwork-202/Day10/A5.c
@@ -5,7 +5,7 @@ int main(){
 int k = 3 , z = 1 temp=0;
  
 printf("k=%d\n",k); 
-int a[]-1,2,3,4,5,6,7,8,9,10,11); int size = sizeof(a) / sizeof(a[0]);
+int a[]=(1,2,3,4,5,6,7,8,9); int size = sizeof(a) / sizeof(a[0]);
  for(int i=0;i<size;i++)
  
 {
