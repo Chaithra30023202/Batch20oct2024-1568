fc9cdfd939fd08b205313c3690f32089ee1e76ae
Chaithra30023202 <chaithrakenchanna15@gmail.com>
2024-11-08 17:32:19 +0530

    Add files via upload

--- Classwork-202/Day10/A4.c
+++ Classwork-202/Day10/A4.c
@@ -2,31 +2,32 @@ A4. #include <stdio.h>
  
 int main(){
  
-int k = 3 , z = 1 temp=0;
+int k = 3 , z = 1,temp=0;
  
 printf("k=%d\n",k); 
-int a[]=(1,2,3,4,5,6,7,8,9); int size = sizeof(a) / sizeof(a[0]);
+int a[]=(1,2,3,4,5,6,7,8,9);
+ int size = sizeof(a) / sizeof(a[0]);
  for(int i=0;i<size;i++)
  
 {
 printf("%d ",a[i]); 
 } 
  
-for(int i = 1 i<=size; i++){
+for(int i = 1; i<=size; i++){
  
 if(i%k==0){
  
 int j=i; 
 int l = k;
- for(int n = 1 n <= (k / 2) ;n++){
- temp=a[j-(1)];
- a[j - (1)] =a[(l^ * z)-1];
- a[(1*z)-1]=temp;
- l--;
+ for(int n = 1 ;n <= (k / 2) ;n++){
+ temp=a[j-(l)];
+ a[j - (l)] =a[(l* z)-1];
+ a[(l*z)-1]=temp;
+ 
  
 }
  
-Z++;
+z++;
 } 
 }
  
